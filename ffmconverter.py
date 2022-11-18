f7a7288fd36aafe5150f07ee3588fdf9175c1e7a
Alex <kozlov.alex.dev@gmail.com>
2022-11-18 10:21:08 +0300

    merging audio + refactor of PATH

--- ffmconverter.py
+++ ffmconverter.py
@@ -1,8 +1,15 @@
 import subprocess
+BASE_FOLDER = "downloads/"
 
 
 def convert_webm_to_mp4(input_file, output_file):
-    # ffmpeg -i test.webm test.mp4
+    # ffmpeg -i /downloads/test.webm /downloads/test.mp4
     # try:
-    command = "ffmpeg -i test.webm test.mp4" #+ input_file + ' ' + output_file
+    command = 'ffmpeg -i ' + BASE_FOLDER + ' ' + input_file + ' ' + BASE_FOLDER + ' ' + output_file
+    subprocess.run(command, shell=True)
+
+
+def merge_audio_to_mp4(input_file, output_file):
+    # ffmpeg -i video.mp4 -i audio.wav -c:v copy -c:a aac output.mp4
+    command = "ffmpeg -i test.mp4 -i audio_test.mp4 -c:v copy -c:a aac output.mp4"
     subprocess.run(command, shell=True)
